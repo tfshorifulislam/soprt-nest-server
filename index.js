79841af5fc393a8a8c8471b2d3f1a9ba15ad66c9
tfshorifulislam <tfshorifulislam@gmail.com>
2026-05-18 09:36:21 +0600

    project setup done

---
+++ index.js
@@ -0,0 +1,47 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv')
+const cors = require('cors')
+
+dotenv.config()
+app.use(cors())
+app.use(express.json())
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const port = process.env.PORT
+const uri = process.env.MONGODB_URI;
+
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('server is running')
+})
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
